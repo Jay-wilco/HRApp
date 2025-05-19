8341775de5346d6bbb2f03b87b1b18ca048e68d9
Jay-wilco <jtvilkko@gmail.com>
2025-05-19 21:22:39 +0300

    add comment to main.jsx to make it not red?

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ import this
 import "./index.css";
 import App from "./App.jsx";
+// why is this still red?
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
